1a857c84797dd077d960ad5bfe0ecff0e5acb602
parthanam <parthnautiyal1016@gmail.com>
2025-06-16 04:40:57 +0530

    adding backend dependencies

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import products from './data/products.js';
+const port = 3001;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+}
+);
+app.get('/api/products', (req, res) => {
+  res.json(products);
+}
+);
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404).send({ message: 'Product not found' });
+  }
+}
+);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import Rating from './Rating'
 const Product = ({product})=>{
     return(
-        <Card className="my-3 p-3 rounded">
+        <Card style={{height : '400px', overflow:'auto'}} className="my-3 p-3 rounded">
             <Link to={`/product/${product._id}`}>
             <Card.Img src={product.image} variant='top'/>
             </Link>
