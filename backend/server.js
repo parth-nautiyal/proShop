596d5498a72465334639c812d7db0f96eed78b45
parthanam <parthnautiyal1016@gmail.com>
2025-08-29 06:08:00 +0530

    refactor product routes: move product-related endpoints to a separate router

--- backend/server.js
+++ backend/server.js
@@ -2,27 +2,15 @@ import express from 'express';
 import dotenv from 'dotenv';  
 dotenv.config();
 import connectDB from './config/db.js';
-import products from './data/products.js';
+import productRoutes from './routes/productRoutes.js';
 const port = process.env.PORT || 3001;
 connectDB();
 
 const app = express();
+app.use('/api/products', productRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
-}
-);
-app.get('/api/products', (req, res) => {
-  res.json(products);
-}
-);
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  if (product) {
-    res.json(product);
-  } else {
-    res.status(404).send({ message: 'Product not found' });
-  }
 }
 );
 
