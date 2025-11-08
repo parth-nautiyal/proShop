b8f0d17d32b09a1c5fae5fb8ab28b1b7d74adc9d
parthanam <parthnautiyal1016@gmail.com>
2025-11-09 01:24:32 +0530

    add axios for API requests and implement product fetching in HomeScreen and ProductScreen components

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,20 +1,31 @@
-import React from 'react'
-import { Row, Col} from 'react-bootstrap'
-import products from '../products'
-import Product from '../components/Product'
+import React from "react";
+import { useState, useEffect } from "react";
+import { Row, Col } from "react-bootstrap";
+import axios from "axios";
+import Product from "../components/Product";
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products");
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <div>
       <h1>Latest Products</h1>
-        <Row>
-            {products.map((product) => (
-            <Col sm={12} md={6} lg={4} xl={3}>
-               <Product product={product}/>
-            </Col>
-            ))}
-            </Row>
+      <Row>
+        {products.map((product) => (
+          <Col sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
     </div>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
