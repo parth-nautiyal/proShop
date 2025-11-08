c78516c3e5bf0c81c356f2d12f3f7959cfe7acbf
parthanam <parthnautiyal1016@gmail.com>
2025-11-09 01:48:41 +0530

    refactor product routes and implement product fetching logic in controller

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -12,9 +12,9 @@ const ProductScreen = () => {
 
   useEffect(() => {
     const fetchProduct = async () => {
-        const { data } = await axios.get(`/api/products/${productId}`);
-        setProduct(data);
-    }
+      const { data } = await axios.get(`/api/products/${productId}`);
+      setProduct(data);
+    };
 
     fetchProduct();
   }, [productId]);
