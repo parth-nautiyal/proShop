544dfad41b8332ccbf6e6a3a64650a8113b3aecb
parthanam <parthnautiyal1016@gmail.com>
2025-11-14 06:30:50 +0530

    add apiSlice for managing API interactions with Redux Toolkit

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,14 @@
 //entry point of redux
 import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "./slices/apiSlice";
 
 const store = configureStore({
-    reducer: {}
+    reducer: {
+        [apiSlice.reducerPath]: apiSlice.reducer
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(apiSlice.middleware),
+    devTools: true
 });
 
 export default store;
